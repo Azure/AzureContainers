a446668856857b1ee1df817f479b0e235e902374
hong-revo <hongooi@microsoft.com>
2018-10-23 19:11:45 +0800

    check input arg length

--- R/kubernetes_cluster.R
+++ R/kubernetes_cluster.R
@@ -222,7 +222,7 @@ call_helm <- function(cmd="", ...)
 # generate a file from a character vector to be passed to kubectl
 make_file <- function(file, ext="")
 {
-    if(file.exists(file))
+    if(length(file) == 1 && file.exists(file1))
         return(file)
 
     out <- tempfile(fileext=ext)
