0ee131bc2a48941a9f6a5582f03dca8c0f778aae
Hong Ooi <hongooi@microsoft.com>
2020-05-06 03:24:27 +1000

    Dev (#14)
    * vignette tweak
    
    * bump ver no
    
    * tool tests
    
    * pass cmd args as processx::run expects
    
    * update news

---
+++ tests/testthat/test00_tools.R
@@ -0,0 +1,39 @@
+context("Tools interface")
+
+if(.AzureContainers$docker == "" ||
+   .AzureContainers$dockercompose == "" ||
+   .AzureContainers$kubectl == "" ||
+   .AzureContainers$helm == "")
+    skip("Tests skipped: external tools not found")
+
+test_that("Docker works",
+{
+    cmd <- "--help"
+    obj <- call_docker(cmd)
+    expect_is(obj, "list")
+    expect_identical(obj$cmdline, "docker --help")
+})
+
+test_that("Docker compose works",
+{
+    cmd <- "--help"
+    obj <- call_docker_compose(cmd)
+    expect_is(obj, "list")
+    expect_identical(obj$cmdline, "docker-compose --help")
+})
+
+test_that("Kubectl works",
+{
+    cmd <- "--help"
+    obj <- call_kubectl(cmd)
+    expect_is(obj, "list")
+    expect_identical(trimws(obj$cmdline), "kubectl --help")
+})
+
+test_that("Helm works",
+{
+    cmd <- "--help"
+    obj <- call_helm(cmd)
+    expect_is(obj, "list")
+    expect_identical(trimws(obj$cmdline), "helm --help")
+})
